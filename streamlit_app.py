00818a7f14a43f6a32c93f9779f3fd7c27469c69
ifuckdata <157847231+ifuckdata@users.noreply.github.com>
2024-02-19 02:01:10 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,38 +3,106 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 
-"""
-# Welcome to Streamlit!
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+channel_name = "nicoleforcadela"
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+import os
+import subprocess
+from datetime import datetime
+import re
+import json
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
+import asyncio
+import time
+from playwright.async_api import async_playwright
+from bs4 import BeautifulSoup
+import pandas as pd
 
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
+def format_date(input_date):
+    date_obj = datetime.strptime(input_date, '%m/%d/%Y')
+    formatted_date = date_obj.strftime('%d %b %Y')
+    return formatted_date
 
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
+def clean_string(input_string):
+    cleaned_string = re.sub(r'[^A-Za-z0-9\s-]', '', input_string)
+    return cleaned_string
 
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
 
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+async def get_page_content(url):
+    async with async_playwright() as p:
+        browser = await p.chromium.launch()
+        context = await browser.new_context()
+        page = await context.new_page()
+
+        await page.goto(url)
+        time.sleep(10)
+        page_content = await page.content()
+
+        await browser.close()
+
+        return page_content
+
+
+def get_vod_list_from_html(html_content):
+    soup = BeautifulSoup(html_content, 'html.parser')
+    vod_list = []
+
+    table = soup.find('table', {'id': 'twitch-grouped'})
+    rows = table.find_all('tr')
+
+    for row in rows:
+        cells = row.find_all('td')
+        if len(cells) >= 5:
+            vod = {
+                'title': clean_string(cells[1].text),
+                'vod_link': cells[1].find('a')['href'],
+                'date': format_date(cells[2].text),
+                'duration': cells[4].text
+            }
+            vod_list.append(vod)
+
+    for i in range(len(vod_list)):
+        vod_list[i]["index"]=i
+
+    return vod_list
+
+
+async def get_twitch_vods_html(channel_name):
+    page_content = await get_page_content(f'https://vodarchive.com/?username={channel_name}')
+    vod_list = get_vod_list_from_html(page_content)
+    return vod_list
+
+
+def run_command(command):
+    process = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
+    output, _ = process.communicate()
+    return output.decode('utf-8')
+
+
+def generate_vod_name(vod_list, index):
+    vod = vod_list[index]
+    name = f'{vod["date"]} {vod["title"]}'.strip()
+    return name
+
+def download_vod(vod_list, index, quality):
+    vod_url = vod_list[index]["vod_link"]
+    vod_m3u8 = run_command(f"streamlink --url {vod_url} --json")
+    vod_m3u8 = json.loads(vod_m3u8)
+    vod_m3u8 = vod_m3u8['streams'][quality]['url']
+    vod_m3u8 = vod_m3u8.split("/")[:-1]
+    vod_m3u8 = "/".join(vod_m3u8)
+    vod_m3u8 = f"{vod_m3u8}/index-dvr.m3u8"
+    print(vod_m3u8)
+    name = generate_vod_name(vod_list, index)
+    os.system(f"ffmpeg -i {vod_m3u8} -c copy ./'{name}.mp4'")
+
+
+
+#channel_name = 'sapphiresings_ph'
+available_vods = await get_twitch_vods_html(channel_name)
+
+available_vods_df = pd.DataFrame(available_vods)
+pd.options.display.max_rows = None
+
+st.write("hello")
+st.dataframe(available_vods_df)
